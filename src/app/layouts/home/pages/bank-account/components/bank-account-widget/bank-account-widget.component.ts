8957d8220bc76569f9decb386a6aea2b3d4a8e81
Francisco Javier González del Solar <frangdelsolar@gmail.com>
2023-08-16 21:31:46 -0300

    balance

--- src/app/layouts/home/pages/bank-account/components/bank-account-widget/bank-account-widget.component.ts
+++ src/app/layouts/home/pages/bank-account/components/bank-account-widget/bank-account-widget.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BankAccountService } from '@app/core/controllers/bank-account-controller.service';
 import { BankAccount } from '@app/core/models/bank-account.interface';
 import { AppDialogService } from '@app/core/services/app-dialog.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-bank-account-widget',
@@ -11,6 +12,8 @@ import { AppDialogService } from '@app/core/services/app-dialog.service';
 export class BankAccountWidgetComponent implements OnInit {
   items: BankAccount[];
 
+  chartCtx: BehaviorSubject<any> = new BehaviorSubject({});
+
   constructor(
     private bankSvc: BankAccountService,
     private dialogSvc: AppDialogService
@@ -19,6 +22,28 @@ export class BankAccountWidgetComponent implements OnInit {
   ngOnInit(): void {
     this.bankSvc.getAll().subscribe((res) => {
       this.items = res.accounts;
+      let chartData = res.historic;
+      let data = {
+        labels: chartData.labels,
+        datasets: [
+          {
+            label: 'Balance',
+            data: chartData.data,
+            fill: true,
+            backgroundColor: 'rgba(100, 180, 255, 0.1)',
+            borderColor: 'rgba(100, 180, 255, 1)',
+            tension: 0,
+          },
+        ],
+      };
+      chartData = {
+        type: 'line',
+        data: data,
+        options: {
+          pointRadius: 0,
+        },
+      };
+      this.chartCtx.next(chartData);
     });
   }
 }
